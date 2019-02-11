dc67bc2c28ae75e5408f3135c9dc5adb24e56046
fedeferreyra <Fe36944562>
2019-02-11 04:57:22 -0300

    Adding swagger

--- src/main/java/com/fferr10/melichallenge/MeliChallengeApplication.java
+++ src/main/java/com/fferr10/melichallenge/MeliChallengeApplication.java
@@ -1,18 +1,39 @@
 package com.fferr10.melichallenge;
 
 import com.fferr10.melichallenge.solar.system.Satellite;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
-public class MeliChallengeApplication {
+public class MeliChallengeApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MeliChallengeApplication.class, args);
 
         context.getBean(Satellite.class).predictWeather();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fferr10.melichallenge.solar.system.controller"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
     }
 
